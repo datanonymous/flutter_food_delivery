3af5a5e4617e790faca769d984efb79a57e89d8e
Alex Ko <ko.e.alexander@gmail.com>
2020-08-19 22:25:33 -0400

    8/19/2020

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,16 +15,55 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: IconButton(
           icon: Icon(Icons.account_circle),
           iconSize: 30,
-          onPressed: (){},
+          onPressed: () {},
         ),
         actions: [
           FlatButton(
-            child: Text('Cart (${currentUser.cart.length})'),
-            onPressed: (){},
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(width: .8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    width: .8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {},
+                ),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
-
